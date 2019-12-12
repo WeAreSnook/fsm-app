27f6f4c17b27b230972edf28fdd3f1803edc5b18
Michael Hayes <michael@addjam.com>
2019-12-12 16:11:23 +0000

    Add working form

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
 const { ipcRenderer } = require('electron')
+const { Application, Controller } = require('stimulus')
+const application = Application.start()
 
-document.getElementById('pickFile').addEventListener('click', () => {
-  ipcRenderer.send('pick-file')
+document.getElementById('run').addEventListener('click', () => {
+  ipcRenderer.send('generate')
 })
+
+application.register(
+  'picker',
+  class extends Controller {
+    static targets = ['hint']
+
+    connect() {
+      ipcRenderer.on(`picked-${this.name}`, this.handlePickResult)
+    }
+
+    pickFile(event) {
+      ipcRenderer.send('openFile', this.name)
+    }
+
+    pickFolder(event) {
+      ipcRenderer.send('openFolder', this.name)
+    }
+
+    get name() {
+      return this.data.get('name')
+    }
+
+    handlePickResult = (event, fileName) => {
+      this.hintTarget.innerText = fileName
+    }
+  }
+)
--- main.js
+++ main.js
@@ -1,40 +1,117 @@
 const { app, BrowserWindow, ipcMain, dialog } = require('electron')
+const { exec } = require('child_process')
+const path = require('path')
 
 // Keep a global reference of the window object, to prevent close on GC
 let win
 
 function createWindow() {
   win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 475,
+    height: 800,
     webPreferences: {
       nodeIntegration: true
     },
     icon: __dirname + '/public/icons/png/icon.png'
   })
 
+  let outputDir = '/'
+  const paths = {
+    output: null,
+    benefitExtract: null,
+    dependents: null,
+    universalCredit: null,
+    awards: null,
+    schoolRoll: null,
+    consent: null,
+    filter: null,
+    benefitAmount: 610.0,
+    rollover: false
+  }
+
   win.loadFile('index.html')
 
   // Open the DevTools.
   // TODO dev only
-  // win.webContents.openDevTools()
+  win.webContents.openDevTools()
 
   win.on('closed', () => {
     win = null
   })
 
-  // create add todo window
-  ipcMain.on('pick-file', () => {
+  ipcMain.on('openFile', (event, name) => {
     const options = {
-      title: 'Choose a file',
+      title: 'Pick a File',
       buttonLabel: 'Select',
       filters: [{ extensions: ['xlsx', 'csv', 'xls'] }]
     }
 
-    const selectedPaths = dialog.showOpenDialog(null, options, filePaths => {
-      event.sender.send('open-dialog-paths-selected', filePaths)
+    dialog
+      .showOpenDialog(win, options, files => {
+        resolve(files ? files[0] : undefined)
+      })
+      .then(file => {
+        if (file == null || file.filePaths.length === 0) {
+          return
+        }
+
+        const filePath = file.filePaths[0]
+        const fileName = path.basename(filePath)
+        paths[name] = filePath
+        event.reply(`picked-${name}`, fileName)
+      })
+  })
+
+  ipcMain.on('openFolder', (event, name) => {
+    const options = {
+      title: 'Pick a Folder',
+      buttonLabel: 'Select',
+      properties: ['openDirectory']
+    }
+
+    dialog
+      .showOpenDialog(win, options, files => {
+        resolve(files ? files[0] : undefined)
+      })
+      .then(file => {
+        if (file == null || file.filePaths.length === 0) {
+          return
+        }
+
+        const filePath = file.filePaths[0]
+        const dirName = path.basename(path.dirname(filePath))
+        paths[name] = filePath
+        event.reply(`picked-${name}`, dirName)
+      })
+  })
+
+  ipcMain.on('generate', event => {
+    // TODO validate no options are null
+    const options = {
+      type: 'info',
+      title: 'Generating Report',
+      buttonLabel: 'Ok'
+    }
+    dialog.showMessageBox(win, options)
+
+    // TODO .exe on windows
+    const paths = {
+      output: null,
+      benefitExtract: null,
+      dependents: null,
+      universalCredit: null,
+      awards: null,
+      schoolRoll: null,
+      consent: null,
+      filter: null,
+      benefitAmount: 610.0,
+      rollover: false
+    }
+    const command = `./fsm-processor --output=${paths.output} --awards=${paths.awards}  --benefitextract=${paths.benefitExtract} --dependents=${paths.dependents} --universalcredit=${paths.universalCredit} --awards=${paths.awards} --schoolroll=${paths.schoolRoll} --consent=${paths.consent} --filter=${paths.filter} --benefitamount=${paths.benefitAmount} --rollover=${paths.rollover}`
+
+    exec(command, (err, stdout, stderr) => {
+      console.log(err, stdout, stderr)
     })
-    console.log(selectedPaths)
   })
 }
 
